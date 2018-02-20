3c3d5bd64835c5ab4d0c40427f3f65bfaf784e7f
Rojazefi <rzegarra@dlya.com.uy>
2018-02-20 14:29:49 -0500

    URL Fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://ronzegarra.github.io/Vue.js-MusicProject/dist/logo.png')
     h1 Platzi Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
