25c8b487813f1c41b63c7829fc40eb28067a3646
Rojazefi <rzegarra@dlya.com.uy>
2018-02-20 14:23:11 -0500

    Initial production commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appname: 'musicProject',
+  apiKey: '18f04d94c1cfd41ed479711e4872f1ed',
+  secret: '163e3a1fa3eb2a6d06ca86fc17f961f9',
+  registeredTo: 'ronzegarra'
+}
+
+
+// Application name	musicProject
+// API key	18f04d94c1cfd41ed479711e4872f1ed
+// Shared secret	163e3a1fa3eb2a6d06ca86fc17f961f9
+// Registered to	ronzegarra
